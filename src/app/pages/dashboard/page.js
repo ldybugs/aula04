7d7a08edc8e45d558d1bf5d21a81e89810d1b43b
ldybugs <vitoriaoliveiramedeiros2006@gmail.com>
2023-08-15 17:37:29 -0400

    commit inicial

--- src/app/pages/dashboard/page.js
+++ src/app/pages/dashboard/page.js
@@ -1,11 +1,16 @@
 import { getUsers } from "@/app/functions/handlerAcessAPI";
 
 export default async function Dashboard() {
+    const lista = getUsers()
    
     return (
         <div>
             <h1>Dashboard</h1>
-            
+            <div>
+               {lista.map((user) =>
+                <h1>{user.name}</h1>
+               )}
+            </div>
         </div>
     );
 };
--- src/middleware.js
+++ src/middleware.js
@@ -13,6 +13,13 @@ export const middleware = (request) => {
             return NextResponse.redirect(urlLogin);
         }
     }
+
+    if (isTokenValidated || token) {
+        if (request.nextUrl.pathname === '/') {
+            return NextResponse.redirect('/pages/dashboard');
+        }
+    }
+
     NextResponse.next();
 };
 export const config = {
